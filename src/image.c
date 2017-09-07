40e105c869d9a2a85d4396e42846e95b550671ea
Nathan McLean <nmclean@kinsolresearch.com>
2017-09-07 14:47:21 -0700

    modification to allow invalid image handling for darknet python server

--- src/image.c
+++ src/image.c
@@ -1296,7 +1296,8 @@ image load_image_stb(char *filename, int channels)
     unsigned char *data = stbi_load(filename, &w, &h, &c, channels);
     if (!data) {
         fprintf(stderr, "Cannot load image \"%s\"\nSTB Reason: %s\n", filename, stbi_failure_reason());
-        exit(0);
+        //exit(0);
+        return make_image(0, 0, 0);
     }
     if(channels) c = channels;
     int i,j,k;
